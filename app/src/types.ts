0cc8b3a732aaa74a59a2c21f15928d3b628047ea
Todsawat Somtua <todsawat.st@mail.kmutt.ac.th>
2022-11-08 00:50:47 +0700

    Made types and constants to Single Source of Truth

--- app/src/types.ts
+++ app/src/types.ts
@@ -1,13 +1,20 @@
+import type { restaurantStatusList, dietList } from '@/constants'
+
 export type Restaurant = {
   id: String
   name: String
   address: String
   website: String
-  status: String
+  status: Status
+  diet?: Diet
 }
 
 export type Dish = {
   id: String
   name: String
-  status: String
+  status: Status
+  diet?: Diet
 }
+
+type Status = typeof restaurantStatusList[number]
+type Diet = typeof dietList[number]
